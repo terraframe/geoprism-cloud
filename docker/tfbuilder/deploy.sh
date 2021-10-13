cd22587ff676bab61778b4d74a4341def3ef3aa2
rrowlands <rrowlands@terraframe.com>
2021-10-13 12:07:03 -0600

    Bugfix for last commit

--- docker/tfbuilder/deploy.sh
+++ docker/tfbuilder/deploy.sh
@@ -5,7 +5,7 @@ set -e
 
 docker build -t tfbuilder .
 
-docker tag tfbuilder:nonroot 961902606948.dkr.ecr.us-west-2.amazonaws.com/tfbuilder:nonroot
+docker tag tfbuilder:latest 961902606948.dkr.ecr.us-west-2.amazonaws.com/tfbuilder:nonroot
 
 aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 961902606948.dkr.ecr.us-west-2.amazonaws.com
 docker push 961902606948.dkr.ecr.us-west-2.amazonaws.com/tfbuilder:nonroot
