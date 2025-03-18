fbdf2839fcf594e57884114d31f92405b900b497
Richard Rowlands <rrowlands@terraframe.com>
2025-03-18 10:27:55 -0600

    Support for tfbuilder tags

--- docker/tfbuilder/deploy.sh
+++ docker/tfbuilder/deploy.sh
@@ -3,9 +3,15 @@
 
 set -e
 
-docker build -t tfbuilder .
+docker build -t tfbuilder:$1 .
 
-docker tag tfbuilder:latest 961902606948.dkr.ecr.us-west-2.amazonaws.com/tfbuilder:latest
+docker tag tfbuilder:$1 961902606948.dkr.ecr.us-west-2.amazonaws.com/tfbuilder:latest
 
 aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 961902606948.dkr.ecr.us-west-2.amazonaws.com
+
+if [ "$1" != "latest" ]; then
+  docker tag tfbuilder:$1 961902606948.dkr.ecr.us-west-2.amazonaws.com/tfbuilder:latest
+  docker push 961902606948.dkr.ecr.us-west-2.amazonaws.com/tfbuilder:$1
+fi
+
 docker push 961902606948.dkr.ecr.us-west-2.amazonaws.com/tfbuilder:latest
